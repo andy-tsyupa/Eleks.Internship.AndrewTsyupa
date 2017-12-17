3359b9cfde2cbb44fc1d3310a103f0e2cc75a0a1
AndrewTsyupa <andy.tsyupa@gmail.com>
2017-12-17 20:50:24 +0200

    17.12.2017

--- src/com/andytsyupa/main/Main.java
+++ src/com/andytsyupa/main/Main.java
@@ -1,4 +1,6 @@
-package com.andytsyupa.main;
+
+
+package vikno;
 
 import org.omg.CORBA.Object;
 
@@ -19,102 +21,117 @@ public class Main {
 
 
     public static ArrayList<File> listFilesForFolder(final File folder, ArrayList<File> list) {
-        int a = 0;
-        for (final File fileEntry : folder.listFiles()) {
 
-            if (fileEntry.isDirectory()) {
-                list = listFilesForFolder(fileEntry, list); //рекурсія//
+        int a = 0;
+        for (final File fileEntry : folder.listFiles()){
+            if ( fileEntry.isDirectory () ) {
+                list=listFilesForFolder ( fileEntry, list ); //рекурсія//
             } else {
-                list.add(fileEntry);
+                list.add ( fileEntry );
             }
         }
         return list;
     }
 
-  /*  public void parse(String path, String startDate, String endDate){
+    public void parse(String path, String startDate, String endDate) {
 
-    }*/
+    }
 
 
     public static void main(String args[]) throws Exception {
 
-        String username = System.getProperty("user.name");
+        String username = System.getProperty ( "user.name" );
 
-        System.out.println(username);
+        System.out.println ( username );
 
         String start = "2017-11-28";
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-        Date startDate = format.parse(start);
+        DateFormat format = new SimpleDateFormat ( "yyyy-MM-dd", Locale.US );
+        Date startDate = format.parse ( start );
 
         String endDate = "2017-11-30";
-        DateFormat format3 = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-        Date endDateFile = format3.parse(endDate);
+        DateFormat format3 = new SimpleDateFormat ( "yyyy-MM-dd", Locale.US );
+        Date endDateFile = format3.parse ( endDate );
 
-        //String path = "D:/Музика";
-        String path = "d:/test";
+        String path = "D:/test";
 
-        File folder = new File(path);
+
+        recursivDelete ( new File ( path ) );
+
+        File folder = new File ( path );
 
         int a = 0;
 
-        ArrayList<File> list = new ArrayList<File>();
 
-        list = listFilesForFolder(folder, list);
+        ArrayList<File> list = new ArrayList<File> ();
+
+        list = listFilesForFolder ( folder, list );
 
 
         long total = 0;
         // bbb.png
-        //     ee.png
+        // ee.png
 
-        //for (File fileEntry : list) {
-        for (int i = 0; i < list.size(); i++) {
-            File fileEntry = list.get(i);
-            total = total + fileEntry.length() / 1048576;
+        for (File fileEntry : list) {
 
-            Date date = new Date();
-            date.setTime(fileEntry.lastModified());
+       /* for (int i = 0; i < list.size (); i++) {
+            File fileEntry = list.get ( i ); //fileEnteri файл
+            total = total + fileEntry.length () / 1048;/*576*/
 
-            if (startDate.compareTo(date) < 0) {
-                fileEntry.delete();
+            Date date = new Date ();
+            date.setTime ( fileEntry.lastModified () );
+
+            if ( startDate.compareTo ( date ) < 0 ) {
+                //fileEntry.delete ();
 
 
-                SimpleDateFormat format1 = new SimpleDateFormat("d-M-Y H:m:s");
-                String DateToStr = format1.format(date);
+                SimpleDateFormat format1 = new SimpleDateFormat ( "d-M-Y H:m:s" );
+                String DateToStr = format1.format ( date );
 
-                System.out.println(fileEntry.getName() + " " + DateToStr);
+                System.out.println ( fileEntry.getName () + " " + DateToStr );
             }
-
-          /*  if (startDate.compareTo(date) > 0){
+        /*
+            if (startDate.compareTo(date) > 0){
                     SimpleDateFormat format2 = new SimpleDateFormat( "d-M-Y H:m:s");
                     String DateToStr = format2.format(date);
 
                     System.out.println(fileEntry.getName() + " " + DateToStr);
-            }*/
+            }
+        */
 
+            System.out.println ( fileEntry.getName () + " " );
 
-            //System.out.println(fileEntry.getName()  +  " " + "Mb");
 
         }
+        System.out.println ( total / 1024 + " " + "Mb" );
 
 
-        System.out.println(total / 1024 + " " + "Gb");
-        try (FileWriter writer = new FileWriter("D:\\Projects\\pracktika\\src\\com\\andytsyupa\\main\\FJava.txt", false)) {
-            String text = "Xeynf";
-            writer.write(text);
-
-            writer.append('\n');
-            writer.append('E'); // дописує
-
-            writer.flush();
+        try (FileWriter writer = new FileWriter ( "D:\\Projects\\pracktika\\src\\com\\andytsyupa\\main\\FJava.xml", false )) {
+            String text = "Час змін:" + " " + "Список видалених файлів:" + " " + "User:";
+            writer.write ( text );
+            writer.append ( '\n' );
+            writer.flush ();
         } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            System.out.println ( ex.getMessage () );
         }
+    }
+
+    public static void recursivDelete(File file) {
+        if ( !file.exists () ) return;
+
+        if ( file.isDirectory () ) {
+            for (File f : file.listFiles ()) {
+                recursivDelete ( f );
+            }
+        }
+        file.delete ();
+        System.out.println ( "Видалення" + file.getAbsoluteFile () );
 
     }
+
 }
 
-
- /*try (FileWriter writer = new FileWriter("D:\\Projects\\pracktika\\src\\com\\andytsyupa\\main\\FJava.txt", false)){
+/*
+ try (FileWriter writer = new FileWriter("D:\\Projects\\pracktika\\src\\com\\andytsyupa\\main\\FJava.xml", false)){
          String text  = "Xeynf";
          writer.write(text);
 
@@ -127,22 +144,7 @@ public class Main {
          catch (IOException ex){
          System.out.println(ex.getMessage());
          }
-        */
-// startdate end
-// enddate
-
-// list
-
-// for list
-
-
-
-
-
-            /*
-            * Код який виводить всі фали із дерикторії.
-            *
-            * */
+*/
 
 
 
